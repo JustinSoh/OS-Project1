17e46d7b186632fa8c644be9a4992af89c89f823
Sean Tay <sean_tay1999@hotmail.com>
2017-06-03 16:30:06 +0800

    Organized imports
    Trying to clear the weird log files or something

--- assignement/src/algorithm/DiskOptimization.java
+++ assignement/src/algorithm/DiskOptimization.java
@@ -3,7 +3,6 @@ package algorithm;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
  
